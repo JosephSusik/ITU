eccbd2d5f8cf8d1eaf156dabd3acaebdd627e5c8
JosephSusik <joseph.susik@gmail.com>
2022-11-26 17:30:13 +0100

    not working get request, need to fix cors policy

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,6 +1,24 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 
 function HomePage() {
+
+
+    useEffect(() => {
+
+        axios
+        .get('http://localhost:8000/listings/', )
+        .then(function(response) {
+            console.log("Response: " + response);
+        })
+        .catch(function(error) {
+            console.log("ERROR: " + error);
+        })
+
+
+    })
+
+
     return(
         <div>
             <p>Homepage</p>
