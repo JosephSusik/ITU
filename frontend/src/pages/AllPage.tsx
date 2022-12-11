ed0cd29e22856f94ce2f819a083b75cdaeba75b7
JosephSusik <joseph.susik@gmail.com>
2022-12-11 12:50:30 +0100

    homepage + pages

--- frontend/src/pages/AllPage.tsx
+++ frontend/src/pages/AllPage.tsx
@@ -7,7 +7,20 @@ function AllPage() {
                 <h2>Všechny inzeráty</h2>
             </div>
             <div className="f_a_b_display">
-
+                <div className="hp_inzerat">
+                    <img src="./img/1/1.jpg" alt="" className="hp_img"/>   
+                    <p className="hp_title">Název</p>
+                    <div className="hp_inzerat_detail_col">
+                        <div className="hp_inzerat_detail_row">
+                            <p className="hp_cat">Category</p>
+                            <p className="hp_place">Place</p>
+                        </div>
+                        <div className="hp_inzerat_detail_row">
+                            <p className="hp_views">Views</p>
+                            <p className="hp_price">Price</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>    
     );
--- frontend/src/pages/FavouritesPage.tsx
+++ frontend/src/pages/FavouritesPage.tsx
@@ -8,7 +8,76 @@ function FavouritesPage() {
                 <h2>Oblíbené inzeráty</h2>
             </div>
             <div className="f_a_b_display">
-
+                <div className="hp_inzerat">
+                    <img src="./img/1/1.jpg" alt="" className="hp_img"/>   
+                    <p className="hp_title">Název</p>
+                    <div className="hp_inzerat_detail_col">
+                        <div className="hp_inzerat_detail_row">
+                            <p className="hp_cat">Category</p>
+                            <p className="hp_place">Place</p>
+                        </div>
+                        <div className="hp_inzerat_detail_row">
+                            <p className="hp_views">Views</p>
+                            <p className="hp_price">Price</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="hp_inzerat">
+                    <img src="./img/1/1.jpg" alt="" className="hp_img"/>   
+                    <p className="hp_title">Název</p>
+                    <div className="hp_inzerat_detail_col">
+                        <div className="hp_inzerat_detail_row">
+                            <p className="hp_cat">Category</p>
+                            <p className="hp_place">Place</p>
+                        </div>
+                        <div className="hp_inzerat_detail_row">
+                            <p className="hp_views">Views</p>
+                            <p className="hp_price">Price</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="hp_inzerat">
+                    <img src="./img/1/1.jpg" alt="" className="hp_img"/>   
+                    <p className="hp_title">Název</p>
+                    <div className="hp_inzerat_detail_col">
+                        <div className="hp_inzerat_detail_row">
+                            <p className="hp_cat">Category</p>
+                            <p className="hp_place">Place</p>
+                        </div>
+                        <div className="hp_inzerat_detail_row">
+                            <p className="hp_views">Views</p>
+                            <p className="hp_price">Price</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="hp_inzerat">
+                    <img src="./img/1/1.jpg" alt="" className="hp_img"/>   
+                    <p className="hp_title">Název</p>
+                    <div className="hp_inzerat_detail_col">
+                        <div className="hp_inzerat_detail_row">
+                            <p className="hp_cat">Category</p>
+                            <p className="hp_place">Place</p>
+                        </div>
+                        <div className="hp_inzerat_detail_row">
+                            <p className="hp_views">Views</p>
+                            <p className="hp_price">Price</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="hp_inzerat">
+                    <img src="./img/1/1.jpg" alt="" className="hp_img"/>   
+                    <p className="hp_title">Název</p>
+                    <div className="hp_inzerat_detail_col">
+                        <div className="hp_inzerat_detail_row">
+                            <p className="hp_cat">Category</p>
+                            <p className="hp_place">Place</p>
+                        </div>
+                        <div className="hp_inzerat_detail_row">
+                            <p className="hp_views">Views</p>
+                            <p className="hp_price">Price</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
