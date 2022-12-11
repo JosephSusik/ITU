2fabb13e50b4482bf1b1df946f06a9ee7fa98e0d
Leopold Nemček <leo.nemcek@gmail.com>
2022-12-11 16:32:14 +0100

    Added global constants file

---
+++ frontend/src/components/Globals.tsx
@@ -0,0 +1,13 @@
+
+export default {
+    BASE_URL: 'http://localhost:3000/',
+    COLORS: {
+        MAIN : "#046a1a",
+        MAIN2: "#4b935a",
+        SECONDARY: "#858585",
+        SECONDARY2: "#c5c5c5",
+        WARNING: "#c2ae45",
+        DANGER: "#c30000"
+
+    } 
+}
